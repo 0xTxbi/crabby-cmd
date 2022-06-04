3ed4eed14d62762029a3ce5435d58664ff592a14
Joseph David <techiejossy@gmail.com>
2022-06-04 20:30:31 +0100

    Define formula

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Welcome to the crabby cmd :)");
+    println!(
+        "Your weight on Mars would be: {}kg",
+        mars_weight_calculator(75.0)
+    );
+}
+
+fn mars_weight_calculator(weight_on_earth: f32) -> f32 {
+    (weight_on_earth / 9.81) * 3.711
 }
